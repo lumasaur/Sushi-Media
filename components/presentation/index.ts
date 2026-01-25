5dbd6804c4f154f0c2d854be96c4c72496009fa7
lumasaur <52591508+lumasaur@users.noreply.github.com>
2026-01-25 11:37:39 -0500

    feat(01-02): add presentation components index
    - Export all 6 presentation components
    - Enables clean imports via @/components/presentation

---
+++ components/presentation/index.ts
@@ -0,0 +1,9 @@
+// Presentation Components
+// Migrated from Kangaroo presentation architecture
+
+export { SlideContainer } from './SlideContainer'
+export { ProgressBar } from './ProgressBar'
+export { SlideCounter } from './SlideCounter'
+export { ScrollIndicator } from './ScrollIndicator'
+export { Breadcrumbs } from './Breadcrumbs'
+export { Logo } from './Logo'
